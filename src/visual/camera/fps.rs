d630adaaf4c361dd6c92f0203250cf4e122902bc
Loke Gustafsson <lokegustafsson@pm.me>
2021-03-22 12:38:43 +0100

    Refactor fps camera and avoid turning upside down

--- src/visual/camera/fps.rs
+++ src/visual/camera/fps.rs
@@ -4,6 +4,11 @@ use winit::event::{ElementState, KeyboardInput, VirtualKeyCode};
 
 const SENSITIVITY: Rad<f32> = Rad(0.0006);
 
+// At [pos], looking towards
+// (angle_equator.cos() * angle_meridian.cos(),
+//  angle_equator.cos() * angle_meridian.sin(),
+//  angle_equator.sin())
+// oriented such that unit_z points as upwards as possible
 pub struct FpsCamera {
     pos: Vector3<f32>,
     angle_equator: Rad<f32>,
@@ -19,7 +24,7 @@ pub struct FpsCamera {
 impl Camera for FpsCamera {
     fn new() -> Self {
         Self {
-            pos: Vector3::unit_x(),
+            pos: -2.0f32 * Vector3::unit_x(),
             angle_equator: Rad::zero(),
             angle_meridian: Rad::zero(),
 
@@ -33,9 +38,9 @@ impl Camera for FpsCamera {
     fn update(&mut self, delta_pos: f32) {
         let velocity = {
             let unit_right =
-                Vector3::new(-self.angle_meridian.sin(), self.angle_meridian.cos(), 0.0);
+                Vector3::new(self.angle_meridian.sin(), -self.angle_meridian.cos(), 0.0);
             let unit_forward =
-                Vector3::new(-self.angle_meridian.cos(), -self.angle_meridian.sin(), 0.0);
+                Vector3::new(self.angle_meridian.cos(), self.angle_meridian.sin(), 0.0);
 
             self.right.f32() * unit_right
                 + self.forwards.f32() * unit_forward
@@ -45,11 +50,18 @@ impl Camera for FpsCamera {
         if velocity.magnitude2() > 0.1 {
             self.pos += velocity.normalize() * delta_pos;
         }
+        // Positive orientation means (aiming right => decrease meridian)
         self.angle_meridian -= SENSITIVITY * self.aim_right;
-        self.angle_equator -= SENSITIVITY * self.aim_up;
+        self.angle_equator += SENSITIVITY * self.aim_up;
 
         self.aim_right = 0.0;
         self.aim_up = 0.0;
+
+        // Avoid turning upside down by clamping angle_equator
+        self.angle_equator = Rad(self
+            .angle_equator
+            .0
+            .clamp(-Rad::<f32>::turn_div_4().0, Rad::turn_div_4().0));
     }
     fn key_input(&mut self, key: KeyboardInput) {
         use VirtualKeyCode::*;
@@ -73,16 +85,22 @@ impl Camera for FpsCamera {
         self.aim_up -= y as f32 - center_y;
     }
     fn camera_to_world(&mut self) -> Matrix4<f32> {
+        let (meridian_sin, meridian_cos) = self.angle_meridian.sin_cos();
+        let (equator_sin, equator_cos) = self.angle_equator.sin_cos();
+
+        // Forwards
         let z_image = Vector3::new(
-            -self.angle_equator.cos() * self.angle_meridian.cos(),
-            -self.angle_equator.cos() * self.angle_meridian.sin(),
-            -self.angle_equator.sin(),
+            equator_cos * meridian_cos,
+            equator_cos * meridian_sin,
+            equator_sin,
         );
-        let x_image = Vector3::new(-self.angle_meridian.sin(), self.angle_meridian.cos(), 0.0);
+        // Right
+        let x_image = Vector3::new(meridian_sin, -meridian_cos, 0.0);
+        // Down
         let y_image = Vector3::new(
-            self.angle_equator.sin() * self.angle_meridian.cos(),
-            self.angle_equator.sin() * self.angle_meridian.sin(),
-            -self.angle_equator.cos(),
+            equator_sin * meridian_cos,
+            equator_sin * meridian_sin,
+            -equator_cos,
         );
         Matrix4::from_cols(
             x_image.extend(0.0),
