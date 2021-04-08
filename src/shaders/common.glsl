ff6172e9242042915e416eccd243ebfecb93c21c
Loke Gustafsson <lokegustafsson@pm.me>
2021-04-08 14:50:39 +0200

    Fix cylinder flickering (robust intersection test)

--- src/shaders/common.glsl
+++ src/shaders/common.glsl
@@ -50,46 +50,10 @@ mat4 solid_get_world_to_local(mat4 solid) {
     return solid;
 }
 
-float hit_time_aabb(const vec3 from, const vec3 ray, vec3 mini, vec3 maxi) {
-    const float EPSILON = 0.001;
+float interact_time(vec2 enterexit) {
+    float enter = enterexit[0];
+    float exit = enterexit[1];
 
-    vec3 times_mini = (mini - from) / ray;
-    vec3 times_maxi = (maxi - from) / ray;
-
-    vec3 times_enter = min(times_mini, times_maxi);
-    vec3 times_exit = max(times_mini, times_maxi);
-
-    float enter = max(times_enter.x, max(times_enter.y, times_enter.z));
-    float exit = min(times_exit.x, min(times_exit.y, times_exit.z));
-
-    if (exit + EPSILON < enter) {
-        return -1;
-    } else if (enter > 0) {
-        return enter;
-    } else {
-        return exit;
-    }
-}
-
-bool are_equal(const float a, const float b, const float epsilon) {
-    return abs(a - b) < epsilon;
-}
-
-// Solve At^2 + 2Bt + C = 0. Return smallest nonnegative or else -1
-float solve_quadratic(float A, float B, float C) {
-    if (A < 0) {
-        A = -A;
-        B = -B;
-        C = -C;
-    }
-    const float det = B*B - A*C;
-    if (det < 0) {
-        return -1;
-    }
-
-    const float sqrtd = sqrt(det);
-    const float enter = (-B - sqrtd)/A;
-    const float exit = (-B + sqrtd)/A;
     if (exit < enter) {
         return -1;
     } else if (enter > 0) {
@@ -99,26 +63,40 @@ float solve_quadratic(float A, float B, float C) {
     }
 }
 
-// max(|x|, |y|, |z|) = 1
-bool on_unit_cube(const vec3 pos) {
-    const float EPSILON = 0.001;
-    vec3 abspos = abs(pos);
-    float maxi = max(abspos.x, max(abspos.y, abspos.z));
-    return are_equal(maxi, 1, EPSILON);
+float hit_time_aabb(const vec3 from, const vec3 ray, vec3 mini, vec3 maxi) {
+    vec3 times_mini = (mini - from) / ray;
+    vec3 times_maxi = (maxi - from) / ray;
+
+    vec3 times_enter = min(times_mini, times_maxi);
+    vec3 times_exit = max(times_mini, times_maxi);
+
+    float enter = max(times_enter.x, max(times_enter.y, times_enter.z));
+    float exit = min(times_exit.x, min(times_exit.y, times_exit.z));
+
+    return interact_time(vec2(enter, exit));
 }
 
-// |z| = 1 and x^2 + y^2 = 1
-bool on_unit_cylinder(const vec3 pos) {
-    const float EPSILON = 0.001;
+bool are_equal(const float a, const float b, const float epsilon) {
+    return abs(a - b) < epsilon;
+}
 
-    bool between_planes = abs(pos.z) < 1 + EPSILON;
-    bool on_planes = are_equal(abs(pos.z), 1, EPSILON);
+// Solve At^2 + 2Bt + C = 0. Return (smallest, largest) solutions
+vec2 solve_quadratic(float A, float B, float C) {
+    if (A < 0) {
+        A = -A;
+        B = -B;
+        C = -C;
+    }
+    const float det = B*B - A*C;
+    if (det < 0) {
+        return vec2(-1, -1);
+    }
 
-    float tube_radius2 = dot(pos.xy, pos.xy);
-    bool on_tube = are_equal(tube_radius2, 1, EPSILON);
-    bool in_tube = tube_radius2 < 1 + EPSILON;
+    const float sqrtd = sqrt(det);
+    const float enter = (-B - sqrtd)/A;
+    const float exit = (-B + sqrtd)/A;
 
-    return (between_planes && on_tube) || (on_planes && in_tube);
+    return vec2(enter, exit);
 }
 
 #endif
--- src/shaders/geometry.glsl
+++ src/shaders/geometry.glsl
@@ -12,36 +12,27 @@ float hit_time_unit_sphere(const vec3 from, const vec3 ray) {
     const float A = dot(ray, ray);
     const float B = dot(ray, from);
     const float C = dot(from, from) - 1;
-    // Solve At^2 + 2Bt + C = 0. Return smallest positive root, else -1.
-    return solve_quadratic(A, B, C);
+    // Solve At^2 + 2Bt + C = 0. Return smallest nonnegative root, else -1.
+    return interact_time(solve_quadratic(A, B, C));
 }
 
 // |z| = 1 and x^2 + y^2 = 1
 float hit_time_unit_cylinder(const vec3 from, const vec3 ray) {
-    float time = 1e9;
-    {
-        const float A = dot(ray.xy, ray.xy);
-        const float B = dot(ray.xy, from.xy);
-        const float C = dot(from.xy, from.xy) - 1;
-        // Solve At^2 + 2Bt + C = 0. Return smallest positive root, else -1.
-        float t = solve_quadratic(A, B, C);
-        if (t > 0 && on_unit_cylinder(from + ray * t)) {
-            time = min(time, t);
-        }
-    }
-    {
-        float t = (-1 - from.z) / ray.z;
-        if (t > 0 && on_unit_cylinder(from + ray * t)) {
-            time = min(time, t);
-        }
-    }
-    {
-        float t = ( 1 - from.z) / ray.z;
-        if (t > 0 && on_unit_cylinder(from + ray * t)) {
-            time = min(time, t);
-        }
-    }
-    return (time == 1e9) ? -1 : time;
+    float plane_t1 = (-1 - from.z) / ray.z;
+    float plane_t2 = ( 1 - from.z) / ray.z;
+
+    vec2 plane = vec2(min(plane_t1, plane_t2), max(plane_t1, plane_t2));
+
+    const float A = dot(ray.xy, ray.xy);
+    const float B = dot(ray.xy, from.xy);
+    const float C = dot(from.xy, from.xy) - 1;
+
+    vec2 tube = solve_quadratic(A, B, C);
+
+    float enter = max(plane[0], tube[0]);
+    float exit = min(plane[1], tube[1]);
+
+    return interact_time(vec2(enter, exit));
 }
 
 // max(|x|, |y|, |z|) = 1
@@ -58,7 +49,7 @@ vec3 normal_unit_sphere(const vec3 pos) {
 
 // |z| = 1 and x^2 + y^2 = 1
 vec3 normal_unit_cylinder(const vec3 pos) {
-    const float EPSILON = 0.001;
+    const float EPSILON = 0.01;
 
     if (are_equal(pos.z, 1, EPSILON)) {
         return vec3(0, 0, 1);
